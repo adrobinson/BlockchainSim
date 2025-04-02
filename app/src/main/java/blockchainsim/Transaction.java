2fcf5a25201a51ddf7b768c66bd2993446011ce8
Adam Robinson <adrobinsonuk25@gmail.com>
2025-04-03 00:19:14 +0100

    Added feature to allow transactions to be signed and verified using wallet private keys

--- app/src/main/java/blockchainsim/Transaction.java
+++ app/src/main/java/blockchainsim/Transaction.java
@@ -19,6 +19,27 @@ public class Transaction {
         this.blockchain = blockchain;
     }
 
+    public void signTransaction(PrivateKey privateKey) throws Exception{
+        String data = sender + receiver + amount; // signature tied to this transaction
+        Signature dsa = Signature.getInstance("SHA256withECDDSA", "BC");
+        dsa.initSign(privateKey);
+        dsa.update(data.getBytes());
+        this.signature = dsa.sign();
+    }
+
+    public boolean verifySignature() throws Exception{
+        String data = sender + receiver + amount; // Recreate transaction data
+        Signature dsa = Signature.getInstance("SHA256withECDSA", "BC"); // Initialize ECDSA, same algorithm we signed with
+
+        // Convert Base64 public key back to actual PublicKey object
+        PublicKey publicKey = KeyFactory.getInstance("ECDSA")
+                .generatePublic(new X509EncodedKeySpec(Base64.getDecoder().decode(sender)));
+
+        dsa.initVerify(publicKey); // Set public key for verification
+        dsa.update(data.getBytes()); // Hash the transaction data
+
+        return dsa.verify(signature); // Check if signature is valid
+    }
 
     public String getSender() {return sender;}
     public String getReceiver() {return receiver;}
